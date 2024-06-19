d4553b73edb177f74dd47d2add22a0ce1e54365c
Josh <joshua.ward@me.com>
2024-06-19 15:00:23 -0600

    delete a bunch of shit

--- apis.js
+++
@@ -1,3 +0,0 @@
-let coors = 'https://cors-anywhere.herokuapp.com/';
-let api = 'https://sandbox-api.brewerydb.com/v2/beers/?key=';
-let apiKey = '25f7a4e2de9f238fb47fd68bbb11df13';
--- cms.js
+++
@@ -1,8 +0,0 @@
-import SERVICE_ENDPOINTS from '../constants/serviceEndpoints';
-import query from './../utils/query';
-
-// eslint-disable-next-line import/prefer-default-export
-export const getCmsContent = (data) => query({
-    endpoint: SERVICE_ENDPOINTS.APW_CMS_CONTENT,
-    data
-});
